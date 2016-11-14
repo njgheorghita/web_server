59e25f5fb5e9d0d4a5968319b2990f5c6e47234d
njgheorghita <njgheorghita@email.wm.edu>
2016-11-14 15:47:34 -0700

    added color output on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,11 +3,12 @@ require 'sinatra/reloader'
 
 class Guess
   attr_reader :number
-  attr_accessor :message
+  attr_accessor :message, :color
 
   def initialize
     @number = rand(100)
     @message = ''
+    @color = "red"
   end
 
   def check_guess(input)
@@ -15,14 +16,19 @@ class Guess
     if (0..100).to_a.include?(input)
       if input > (number + 5)
         @message = "Way too high!"
+        @color = "red"
       elsif input < (number - 5)
         @message = "Way too low!"
+        @color = "red"
       elsif input > number
         @message = "Too high!"
+        @color = "#fc8f7e"
       elsif input < number
         @message = "Too low!"
+        @color = "#fc8f7e"
       else 
         @message = "You got it right! \n The secret number is #{number}"
+        @color = "green"
       end
     else
       @message = "Please guess between 0 - 100"
@@ -35,7 +41,7 @@ instance = Guess.new
 get '/' do 
   guess = params['guess'].to_i
   message = instance.check_guess(guess)
-  erb :index, :locals => {:number => instance.number, :message => instance.message}
+  erb :index, :locals => {:number => instance.number, :message => instance.message, :color => instance.color}
 end
 
 
